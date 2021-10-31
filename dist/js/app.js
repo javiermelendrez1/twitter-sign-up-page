608bd787c3f97e853f0fbd9998eee04be68720c3
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2021-10-31 15:07:34 -0700

    all the html for the modal
    - this is the HTML code I believe I need so far for the Modal

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,5 @@
 console.log('app.js is connected');
+//query for the sign up button
+const signUpButton = document.querySelector('.sign-up-container-links-btn-sign-up').addEventListener('click', () => {
+    console.log('you clicked the sign up button');
+})
